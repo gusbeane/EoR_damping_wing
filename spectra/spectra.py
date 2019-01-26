835df3716bf2caf36e9c088e30124498c0f4b213
gusbeane <abeane@sas.upenn.edu>
2019-01-26 16:26:04 -0500

    Add cosmo attribute to spec

--- spectra/spectra.py
+++ spectra/spectra.py
@@ -5,6 +5,7 @@ import astropy.units as u
 from astropy.table import Table
 
 import matplotlib.pyplot as plt
+from colossus.cosmology import cosmology
 
 class spec(object):
     """Spectrum class with some useful methods.
@@ -14,7 +15,7 @@ class spec(object):
         attr2 (:obj:`int`, optional): Description of `attr2`.
 
     """
-    def __init__(self, wavelength, flux, redshift, continuum=None, logwavelength=False):
+    def __init__(self, wavelength, flux, redshift, continuum=None, logwavelength=False, cosmo=None):
         """Initialize the spectrum object.
 
         Args:
@@ -23,6 +24,7 @@ class spec(object):
             redshift (:obj:`float`): Redshift of emission for the quasar
             continuum (:obj:`list` of :obj:`float`, optional): Array of continuum values (same units as flux)
             logwavelength(:obj:`bool`, optional): If wavelength column is log10(wavelength) (default: False)
+            cosmo(colossus cosmo object, optional): Cosmology object from colossus (default: 'planck18')
         """
         try:
             if logwavelength:
@@ -50,6 +52,11 @@ class spec(object):
         self.lymanseries = {'Lyalpha': 1215.67 * u.AA,
                             'Lybeta': 1025.73 * u.AA,
                             'Lygamma': 972.54 * u.AA}
+
+        if cosmo is not None:
+            self.cosmo = cosmo
+        else:
+            self.cosmo = cosmology.setCosmology('planck18')
 
     def plot(self, show=True):
         fig, ax = plt.subplots(1, 1)
